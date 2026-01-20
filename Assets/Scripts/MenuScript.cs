2093622661a568ffcc18ed3c9e61c21b43dd6115
mattxix <marussas@email.neit.edu>
2026-01-20 14:19:36 -0500

    AddedMenuAndGraphics
    Added simple main menu and player, zombie and gun images

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+   public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
